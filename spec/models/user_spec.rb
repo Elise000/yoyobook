7e79d72625a004be55cbe31e150451af9fc23174
Next <nextacademy@email.com>
2016-06-02 03:27:55 -0700

    user model done

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,30 @@ RSpec.describe User, type: :model do
     it "has a valid factory" do
       expect(FactoryGirl.build(:user)).to be_valid
     end
+
+    it "should have username, email and password" do
+      should have_db_column(:username).of_type(:string)
+      should have_db_column(:email).of_type(:string)
+      should have_db_column(:password).of_type(:string)
+    end
   end
+
+  context "validation" do
+    it { is_expected.to validate_presence_of(:username) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email) }
+    
+    it "is invalid without a lastname" do
+      FactoryGirl.build(:user, password: nil).should_not be_valid
+    end
+
+    it "is invalid without proper email" do
+      FactoryGirl.build(:user, email: "abc").should_not be_valid
+    end
+
+    it "user can sign up when all three parameters is present" do
+      expect(FactoryGirl.build(:user)).to be_valid
+    end
+  end
+
 end
