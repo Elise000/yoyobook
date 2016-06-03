c680f91abd3690d0ce8e240ae68169717bf91aaf
Next <nextacademy@email.com>
2016-06-03 02:22:05 -0700

    working on users_controller specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context "record creation" do
+  context "for record creation" do
     it "has a valid factory" do
       expect(FactoryGirl.build(:user)).to be_valid
     end
@@ -9,7 +9,6 @@ RSpec.describe User, type: :model do
     it "should have username, email and password" do
       should have_db_column(:username).of_type(:string)
       should have_db_column(:email).of_type(:string)
-      should have_db_column(:password).of_type(:string)
     end
   end
 
@@ -18,17 +17,20 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:email) }
     
-    it "is invalid without a lastname" do
-      FactoryGirl.build(:user, password: nil).should_not be_valid
+      describe "password format requirement" do
+        it { is_expected.to validate_presence_of(:password) }
+        it { is_expected.to validate_length_of(:password).is_at_least(6).is_at_most(20) }
+        it { is_expected.to validate_confirmation_of(:password) }
+      end
+
+    it "is invalid without proper email format" do
+      expect(FactoryGirl.build(:user, email: "abc")).to be_invalid
     end
 
-    it "is invalid without proper email" do
-      FactoryGirl.build(:user, email: "abc").should_not be_valid
-    end
-
-    it "user can sign up when all three parameters is present" do
+    it "user can sign up when all parameters is present" do
       expect(FactoryGirl.build(:user)).to be_valid
     end
+
   end
 
 end
